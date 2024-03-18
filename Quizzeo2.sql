462fac5b70c59c227b8426cea6e1be180b965aea
Babacar2804 <babacarg836@gmail.com>
2024-03-18 16:39:22 +0100

    ajout

--- Quizzeo2.sql
+++ Quizzeo2.sql
@@ -1,33 +1,39 @@
-DROP DATABASE IF EXISTS Quizzeo2;
-CREATE DATABASE Quizzeo2;
-USE Quizzeo2;
+Drop Database if exists Quizzeo2;
+CREATE Database if not exists Quizzeo2;
+use Quizzeo2;
+CREATE TABLE Rôles(
+   id_role INT PRIMARY KEY AUTO_INCREMENT,
+   admin VARCHAR(50),
+   validateur VARCHAR(50),
+   quizz_admin VARCHAR(50),
+   quizzer VARCHAR(50),
+   utilisateur VARCHAR(50)
+);
 
-DROP TABLE IF EXISTS Users;
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE Users(
    id_user INT PRIMARY KEY AUTO_INCREMENT,
    pseudo VARCHAR(50),
    email VARCHAR(50),
    password VARCHAR(50),
-   role enum('admin','validateur','quizz_admin','quizzer','utilisateur') NOT NULL,
-   statut_compte enum('active','desactive')  NOT NULL
+   statut_compte enum('active','desactive') NOT NULL,
+   id_role INT NOT NULL,
+   FOREIGN KEY(id_role) REFERENCES Rôles(id_role)
 );
 
-DROP TABLE IF EXISTS Quizzes;
-CREATE TABLE IF NOT EXISTS Quizzes (
+CREATE TABLE Quizzes(
    id_quizz INT PRIMARY KEY AUTO_INCREMENT,
    titre VARCHAR(50),
    date_creation DATE,
    nbre_questions INT,
-   type enum('QCM','Feedback') NOT NULL,
+   type enum('QCM','Sondage') NOT NULL,
    Description VARCHAR(120),
    statut_quizz enum('creation','lance','termine') NOT NULL,
-   visi_resu enum('public','prive')  NOT NULL,
+   visi_res('public','prive') NOT NULL,
    id_user INT NOT NULL,
    FOREIGN KEY(id_user) REFERENCES Users(id_user)
 );
 
-DROP TABLE IF EXISTS Questions;
-CREATE TABLE IF NOT EXISTS Questions (
+CREATE TABLE questions(
    id_question INT PRIMARY KEY AUTO_INCREMENT,
    text VARCHAR(150),
    intitule VARCHAR(50),
@@ -35,25 +41,21 @@ CREATE TABLE IF NOT EXISTS Questions (
    FOREIGN KEY(id_quizz) REFERENCES Quizzes(id_quizz)
 );
 
-DROP TABLE IF EXISTS Reponses;
-CREATE TABLE IF NOT EXISTS Reponses (
+CREATE TABLE reponses(
    id_reponse INT PRIMARY KEY AUTO_INCREMENT,
    text_reponse VARCHAR(50),
-   statut_reponse enum('correcte','incorrecte') NOT NULL,
+   statut_reponse enum('correct','incorrect') NOT NULL,
    id_question INT NOT NULL,
-   FOREIGN KEY(id_question) REFERENCES Questions(id_question)
+   FOREIGN KEY(id_question) REFERENCES questions(id_question)
 );
 
-DROP TABLE IF EXISTS Reponse_user;
-CREATE TABLE IF NOT EXISTS Reponse_user (
+CREATE TABLE reponse_user(
    id_repuser INT PRIMARY KEY AUTO_INCREMENT,
    id_user INT,
    id_question INT,
    id_reponse INT,
-   statut_rep enum('correcte','incorrecte') NOT NULL,
+   statut_rep enum('correct','incorrect') INT NOT NULL,
    FOREIGN KEY(id_user) REFERENCES Users(id_user),
-   FOREIGN KEY(id_question) REFERENCES Questions(id_question),
-   FOREIGN KEY(id_reponse) REFERENCES Reponses(id_reponse)
-);
-
-
+   FOREIGN KEY(id_question) REFERENCES questions(id_question),
+   FOREIGN KEY(id_reponse) REFERENCES reponses(id_reponse)
+)
