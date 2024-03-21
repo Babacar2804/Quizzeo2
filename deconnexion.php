e775a6bc90d1ac70aa8b7c233a5f0a2022eaa208
Numen <stivegamy@outlook.fr>
2024-03-21 15:25:40 +0100

    classes

---
+++ deconnexion.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+require 'classes.php';
+$db = new BDD();
+
+if (isset($_SESSION['user_id'])) {
+    $adminSite = new AdminSite($db);
+    $adminSite->updateStatus($_SESSION['user_id'], "inactive");}
+session_destroy();
+header("Location: connexion.php");
+exit();
+?>
