999ae7544e8b4755eb41b8ddae5321bfa7421da8
poulpy666 <136070053+poulpy666@users.noreply.github.com>
2024-03-21 15:21:38 +0100

    ajout

--- D&co/inscriptiond&co.php
+++ D&co/inscriptiond&co.php
@@ -11,19 +11,18 @@
 <body>
     <?php include 'navbar.html'; ?><br><br>
     <div class="principal">
-        <div class="container">
-            <br><br>
-            <div class="inscription">
-                <h1>Inscription</h1>
-                <form method="post" id="registrationForm" action="#">
-                    <input type="text" id="pseudo" name="pseudo" placeholder="pseudo" required></input><br><br>
-                    <input type="email" id="email" name="email" placeholder="email" required></input><br><br>
-                    <input type="password" id="password" name="password" placeholder="mot de passe"required></input><br><br>
-                    <img src="\Quizzeo2\captcha.php"/><br><br>
-                    <input type="text" name="captcha" placeholder="rentrez le captcha"></input><br><br>
-                    <input type="submit" id="submit" name="submit" value="S'inscrire"></input>
-                </form>
-            </div>
+        <br><br>
+        <div class="inscription">
+            <h1>Inscription</h1>
+            <form method="post" id="registrationForm" action="#">
+                <input type="text" id="pseudo" name="pseudo" placeholder="pseudo" required></input><br><br>
+                <input type="email" id="email" name="email" placeholder="email" required></input><br><br>
+                <input type="password" id="password" name="password" placeholder="mot de passe"
+                    required></input><br><br>
+                <img src="\Quizzeo2\captcha.php" /><br><br>
+                <input type="text" name="captcha" placeholder="rentrez le captcha"></input><br><br>
+                <input type="submit" id="submit" name="submit" value="S'inscrire"></input>
+            </form>
         </div>
     </div>
 </body>
