91a941590369a4105d5ae7b933c7662323a9b45a
Numen <stivegamy@outlook.fr>
2024-03-21 15:32:55 +0100

    classes

--- connexion.php
+++ connexion.php
@@ -2,7 +2,6 @@
 session_start();
 
 require 'classes.php';
-
 $db = new BDD();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])){
@@ -15,10 +14,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])){
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['user'] = $user['pseudo'];
+        $_SESSION['user_pseudo'] = $user['pseudo'];
         $_SESSION['user_id'] = $user['id_user'];
         $_SESSION['user_role'] = $user['id_role'];
-
+    
+        $adminSite = new AdminSite($db);
+        $adminSite->updateStatus($user['id_user'], "active");    
         switch ($user['id_role']) {
             case 1: 
                 header("Location: admin.php");
