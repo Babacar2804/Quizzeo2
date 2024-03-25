3fe054aab3805b23a80fc83fe49c623933c29c93
Babacar2804 <babacarg836@gmail.com>
2024-03-25 12:10:42 +0100

    ajout

--- quizzes.sql
+++ quizzes.sql
@@ -16,10 +16,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-DROP Database if EXISTS Quizzeo;
-CREATE DATABASE quizzeo2
+DROP Database if EXISTS quizzeo;
+CREATE DATABASE quizzeo
 
-use quizzeo2;
+use quizzeo;
 --
 -- Database: `quizzeo2`
 --
